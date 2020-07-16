e1ef952b2f2ddc21b1354fd5ddcbd9371ac262a1
Himanshu Bansal <himanshubansal155@gmail.com>
2020-07-16 19:02:12 +0530

    first commit

---
+++ numberToWords.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+
+public class numberToWords {
+    public static void main(final String[] args) {
+        final Scanner scanner = new Scanner(System.in);
+        final int number = scanner.nextInt();
+        scanner.close();
+        System.out.println(number);
+        Word show=new Word();
+        show.buy((number % 100));
+    }
+}
+class Word{
+    
+    void buy(int n){
+        String one[] = {"","One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten",
+        "Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen"};
+
+        String ten[] = {"","Twenty","Thirty","Forty","Fifty","Sixty","Seventy","Eighty","Ninety"};
+
+        if (n > 19)
+        {
+        System.out.print(ten[(n/10)-1]+" " +one[n%10]);
+        }
+        else
+        {
+        System.out.print(one[n]);
+        }
+    }
+    
+}
+ 
